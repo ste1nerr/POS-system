94f150c33f68ce6f35d2b4e98443b2ed565ea38a
ste1nerr <oachkevych@gmail.com>
2022-12-13 17:57:23 +0200

    post request

--- server/index.js
+++ server/index.js
@@ -1,17 +1,34 @@
-import e from "express";
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
+
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('hello');
 });
 
+app.post('/auth/login', (req, res) => {
+    console.log(req.body);
+
+    const token = jwt.sign(
+        {
+        email: req.body.email, 
+        fullname: 'vasya' 
+        }, 'secret123');
+
+
+    res.json({
+        success:true, 
+        token,
+    });
+});
 
 app.listen(3000, (err) => {
     if (err) {
         return console.log(err);
     }
-
     console.log('server ok')
 });
