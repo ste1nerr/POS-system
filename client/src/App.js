05f9cb4d362cbbfbfb84b34c7bd98eca8632b9c9
ste1nerr <oachkevych@gmail.com>
2023-01-11 11:51:37 +0200

    router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Router, Link } from 'react-router'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={}/>
+        <Route path="auth" element={}/>
+        <Route path="register" element={}/>
+        <Route path="admin_panel" element={}/>
+      </Routes>
+    </>
   );
-}
+} 
 
 export default App;
